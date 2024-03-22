d749816e8595bc305980ba611b7ea4aaaf0f888b
asedemeow <155187284+asedemeow@users.noreply.github.com>
2024-03-22 07:20:02 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-9c595152-9378-464c-9d90-064b34bfe78a/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-2f9274e9-38b5-448b-a1a3-c1f24adc1e08/)
 
 
 echo "$curl_output" >> Update.tsx
